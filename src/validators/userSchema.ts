90d34f83ae501618e402b9a5586e7575314407f9
Shubham Kumar <malhotrashubham48@gmail.com>
2024-08-20 11:07:54 +0530

    zod prompts init

--- src/validators/userSchema.ts
+++ src/validators/userSchema.ts
@@ -1,17 +1,25 @@
 import { z } from 'zod'
 
-
 export const registerSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
+    email: z.string().email({
+        message: 'Invalid email address'
+    }),
+    password: z.string().min(6, {
+        message: 'Password must be at least 6 characters long'
+    }),
 });
 
 export const loginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6),
+    email: z.string().email({
+        message: 'Invalid email address'
+    }),
+    password: z.string().min(6, {
+        message: 'Password must be at least 6 characters long'
+    }),
 });
 
 export const resetPasswordSchema = z.object({
-    email: z.string().email()
+    email: z.string().email({
+        message: 'Invalid email address'
+    }),
 });
-
